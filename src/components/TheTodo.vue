f49e0d53ef3ba940a7af631906a3d92bf356d52c
OziOcb <pozyzniewski@yahoo.com>
2022-05-01 20:17:36 +0100

    Add html

--- src/components/TheTodo.vue
+++ src/components/TheTodo.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Elo</div>
+  <ul class="list"></ul>
+
+  <form class="newTaskForm">
+    <input type="text" class="newTaskForm__input" />
+    <button type="submit" class="newTaskForm__button">Add</button>
+  </form>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.list {
+  list-style: none;
+  padding: 0;
+}
+</style>
